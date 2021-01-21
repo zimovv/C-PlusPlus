1e51216f723958691a408d3f22f633be675d20fd
zimovv <781422258@qq.com>
2021-01-21 17:12:15 +0800

    test if there is any way to create another instance
    3 way to create an instance:
    (1) constructor
    (2) copy constructor
    (3) assignment constructor(override operator =)

--- singleton.cpp
+++ singleton.cpp
@@ -25,9 +25,26 @@ Singleton * Singleton::getInstance()
     return m_instance;
 }
 
+class DerivedS : public Singleton
+{
+public:
+    //Singleton * m_instance_d = new Singleton();//‘Singleton::Singleton()’ is private
+};
+
 int main()
 {
     Singleton * s1 = Singleton::getInstance();
     Singleton * s2 = Singleton::getInstance();
+
+    //test if any way can create another instance
+    //Singleton s3; //‘Singleton::Singleton()’ is private
+    //Singleton s4(*s1); //‘Singleton::Singleton(const Singleton&)’ is private
+    //Singleton *s5=nullptr;
+    //*s5 = *s2;//‘Singleton& Singleton::operator=(const Singleton&)’ is private
+
+    //DerivedS d; // not neccessary to create DerivedS object
+
+    delete s1;
+    //delete s2; //double free
     return 0;
 }
